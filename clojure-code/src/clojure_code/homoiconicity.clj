a81cdb2031ebdbbb00e43b301dfeef45e547cba3
Tom Helmuth <thelmuth@hamilton.edu>
2018-10-19 14:24:20 -0400

    Clojure Homoiconicity

--- clojure-code/src/clojure_code/homoiconicity.clj
+++ clojure-code/src/clojure_code/homoiconicity.clj
@@ -1,6 +1,62 @@
 (ns clojure-code.homoiconicity)
 
+
+;; (read-string "(+ 2 3)")
+
+;; (eval (read-string "(+ 2 3)"))
+
+;; (eval '(+ 2 3))
+
+;; (first (read-string "(+ 2 3)"))
+
+;; (type (read-string "(+ 2 3)"))
+
+;; (read-string "[1 2 3]")
+
+;; (read-string "(quote (2 3 4))")
+;; (read-string "'(2 3 4)") ; Reader-macro for quote
+
+;; (read-string "#(+ % %)")
+
+;; (read-string "(+ 2 3);this is a comment")
+
+;; (read-string "variable")
+
+;; '(a b c)
+
+;; eval
+
+;; (eval 5)
+
+;; (let [x 45]
+;;   (eval x))
+
+;; (+ 2 3)
+
+;; '(+ 2 3)
+
+;; (eval '(+ 2 3))
+
+;; (eval '('(* 10 3)))
+
+;; (read-string "'('(* 10 3))")
+
+;; (read-line)
+
+(defn repl
+  "A Clojure REPL"
+  []
+  (print "H]=> ")
+  (flush)
+  (let [line (read-line)]
+    (when (not (empty? line))
+      (println (eval (read-string line)))
+      (recur))))
+
+;(repl)
+
+
 (defn -main
   []
-  (println (+ 8 7)))
+  (repl))
 
