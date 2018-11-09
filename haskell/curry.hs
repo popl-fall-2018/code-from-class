3cfd06ab1c118e5aa26920c53e1ead71ef661d8e
Tom Helmuth <thelmuth@hamilton.edu>
2018-11-09 15:15:49 -0500

    Updated all Haskell code to include command line examples.

--- haskell/curry.hs
+++ haskell/curry.hs
@@ -1,38 +1,85 @@
+-------- Partially Applied Functions --------
 
-mult_then_add :: Num a => a -> (a -> (a -> a))
-mult_then_add x y z = (x * y) + z
+multThenAdd x y z = (x * y) + z
+-- What is the type?
 
-
-mult_by_5_then_add y z = (5 * y) + z
+-- We can use partial application to get new functions
+multBy5ThenAdd y z = (5 * y) + z
 -- equivalent to
-mult_by_5_then_add' y z = mult_then_add 5 y z
+multBy5ThenAdd' y z = multThenAdd 5 y z
 -- equivalent to
-mult_by_5_then_add'' = mult_then_add 5
+multBy5ThenAdd'' = multThenAdd 5
 
+-- We can do the same with built-in functions
 divide100 x = div 100 x
--- equivlent to
+-- equivalent to
 divide100' = div 100
 
-divide_by_14 x = div x 14
-
-divide_by_14' = (`div` 14)
-
 add5 x = x + 5
-
-add5' = (+ 5)
-
-add5'' = (+) 5
+--equivalent to
+add5' = (+ 5)            -- here, 5 is the second argument
 
 equalsA x = x == 'A'
+--equivalent to
+equalsA' = (==) 'A'      -- here, 'A' is the first argument
 
-equalsA' = (== 'A')
+divideBy14 x = div x 14
+--equivalent to
+divideBy14' = (`div` 14) -- here, 14 is the second argument
 
-equalsA'' = (==) 'A'
+-- Any of these partially applied functions can be used as anonymous functions
+-- map (+ 5) [1..10]
+-- filter (10 >) [1..20]
+-- filter (> 10) [1..20]
 
 
+-------- Currying --------
+
+-- This will explain why partially applied functions work
+-- _Currying_ takes a function with multiple arguments and breaks it into
+-- multiple functions with 1 argument each.
+-- (almost) Every function in Haskell really takes only 1 argument
+
+-- :t (+)
+-- (+) :: Num a => a -> a -> a
+-- (+) :: Num a => a -> (a -> a)
+
+-- multThenAdd x y z = (x * y) + z
+-- multThenAdd :: Num a => a -> a -> a -> a
+-- multThenAdd :: Num a => a -> (a -> (a -> a))
+
+-- :t (==)
+-- (==) :: Eq a => a -> a -> Bool
+
+-- So, when you partially apply a function, you are getting back a function
+-- of the remaining arguments.
+
+-- :t map
+-- map :: (a -> b) -> [a] -> [b]
+-- map :: (a -> b) -> ([a] -> [b])
+---- Says that map takes a function from 'a' to 'b' and returns a function
+---- from [a] to [b].
+
+-- mapSquare = map (^ 2)
+-- mapSquare [1, 3, 5, 7]
+-- :t mapSquare
+-- mapSquare :: Num b => [b] -> [b]
+
+-- :t filter
+-- filter :: (a -> Bool) -> [a] -> [a]
+---- filter takes a function from 'a' to Bool and returns a function from [a]
+---- to [a]
 
 
+-------- Anonymous Functions --------
 
-mystery = map (^ 2)
+-- (\x -> x+3)
+---- equivalent to
+-- (+ 3)
+---- So, often just use partially applied functions. But, anonymous functions
+---- are sometimes useful
 
+longWords words = filter (\w -> length w > 5) words
+-- longWords ["hi", "there", "independent", "scafold", "star"]
 
+-- foldl1 (\x y -> (2+x) * (2+y)) [1..4]
