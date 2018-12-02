f493e6935b3dc48f7b8c91869ed84737ce7abb2d
Tom Helmuth <thelmuth@hamilton.edu>
2018-12-02 14:10:29 -0500

    Added Einstein Jr. for class.

---
+++ prolog/einstein_jr.pl
@@ -0,0 +1,8 @@
+
+% Facts:
+% 1. Three people live in 3 houses with 3 colors and 3 pets.
+% 2. The person in the red house has a giraffe.
+% 3. The first house is blue.
+% 4. The house with the elephant is to the left of the green house.
+% 5. Someone owns an emu.
+
